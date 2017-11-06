14e717bbcbcc5ada4d467667b7d1698226409eed
Paul Sztajer <paul@sztajer.org>
2017-11-06 15:27:34 +1100

    adding easy global commit

--- commit.sh
+++ commit.sh
@@ -1,11 +1,27 @@
+file="./simple.dependencies"
+CDIR=${PWD}
+REPOS=()
+
 if [ $1="deps" ]; then
-    cd simple.deps
-    git add *
-    git commit -m "$2"
-    git push origin master
+    REPOS+=("./simple.deps")
 elif [ -d "./Modules/$1" ]; then
-    cd ./Modules/$1
+    REPOS+=("./Modules/$1")
+elif [ $1="all" ]; then
+    REPOS+=("./simple.deps")
+    while IFS= read line
+    do
+        read name repo <<< "$line"
+        REPOS+=("./Modules/$name")
+    done <"$file"
+    REPOS+=("./")
+fi
+
+for repo in "${REPOS[@]}"; do
+    echo $PWD
+    echo $repo
+    cd $repo
     git add *
     git commit -m "$2"
     git push origin master
-fi
+    cd $CDIR
+done
